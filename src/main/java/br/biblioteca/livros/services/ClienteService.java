3a26e22b95c4e9282df5e50dfeee0d8d502e653c
User <User@LABA18-01-PC>
2018-04-28 15:07:09 -0300

    SprintBoot

--- src/main/java/br/biblioteca/livros/services/ClienteService.java
+++ src/main/java/br/biblioteca/livros/services/ClienteService.java
@@ -2,9 +2,12 @@ package br.biblioteca.livros.services;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import br.biblioteca.livros.beans.Cliente;
 import br.biblioteca.livros.repository.ClienteRepository;
 
+@Service
 public class ClienteService {
 
 	@Autowired
